8b751846136e89712eea6f783d5f723728e040f7
entl <maxvorobyov01@gmail.com>
2023-01-08 15:42:35 +0000

    get_comments function implemented

--- reddit.py
+++ reddit.py
@@ -1,34 +1,42 @@
 import praw
-from praw.models import MoreComments
 from settings import CLIENT_ID, CLIENT_SECRET, USER_AGENT, SUBREDDIT
 from exceptions import CantLoginReddit
+from typing import TypeAlias
 
-def login():
-    reddit = praw.Reddit(
+Login_obj: TypeAlias = praw.Reddit
+
+def login() -> Login_obj:
+    login_obj = praw.Reddit(
         client_id = CLIENT_ID,
         client_secret = CLIENT_SECRET,
         user_agent = USER_AGENT,
     )
-
-    if reddit.read_only == False:
+    #Raise error if login was unsuccessful
+    if login_obj.read_only == False:
         raise CantLoginReddit
-    return reddit
+    return login_obj
 
-def get_posts(reddit: praw.Reddit, subreddit:str):
+#get last most popular subreddits based on limit
+def get_posts(reddit: Login_obj, subreddit:str, limit:int = 1) -> list:
     # TODO: Check whether post already in database
-    return reddit.subreddit(subreddit).hot(limit=1)
+    return list(reddit.subreddit(subreddit).hot(limit=limit))
+
+#get comments from subreddit
+def get_comments(reddit: Login_obj, post_id: str) -> list:
+    submission = reddit.submission(id=post_id)
+    #get rid of MoreComments class in submission
+    submission.comments.replace_more(limit=0)
+    submission.comment_sort = "top"
+    comments = submission.comments.list()
+    return comments[:10]
 
 
 if __name__ == "__main__":
     reddit_obj = login()
     # for post in get_posts(reddit=reddit_obj, subreddit=SUBREDDIT):
     #     print(post.title)
-    print(get_posts(reddit=reddit_obj, subreddit=SUBREDDIT))
-
-# url = "https://www.reddit.com/r/funny/comments/3g1jfi/buttons/"
-# submission = reddit.submission(url=url)
-
-# for top_level_comment in submission.comments:
-#     if isinstance(top_level_comment, MoreComments):
-#         continue
-#     print(top_level_comment.body)
+    #     for comment in get_comments(reddit=reddit_obj, post_id=post.id):
+    #         print("------------")
+    #         print(comment.author)
+    #         print(comment.body)
+    #     print(post.title)
