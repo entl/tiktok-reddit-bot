1f8b891f6bed3b018b84712cc45537e7d201db46
entl <maxvorobyov01@gmail.com>
2023-01-08 16:04:35 +0000

    main.py filled, settings reworked

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from syntesize import syntesize
+from reddit import login, get_posts, get_comments
+from settings import CLIENT_ID, CLIENT_SECRET, USER_AGENT, SUBREDDIT
 
 def main():
-    pass
+    login_obj = login(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, user_agent=USER_AGENT)
+    posts = get_posts(login_obj, SUBREDDIT, 1)
+    comment = get_comments(login_obj, posts[0].id, limit=1)
+    syntesize(comment[0].body, filename='test')
 
 if __name__ == "__main__":
     main()
--- reddit.py
+++ reddit.py
@@ -5,11 +5,11 @@ from typing import TypeAlias
 
 Login_obj: TypeAlias = praw.Reddit
 
-def login() -> Login_obj:
+def login(client_id = CLIENT_ID, client_secret = CLIENT_SECRET, user_agent = USER_AGENT) -> Login_obj:
     login_obj = praw.Reddit(
-        client_id = CLIENT_ID,
-        client_secret = CLIENT_SECRET,
-        user_agent = USER_AGENT,
+        client_id = client_id,
+        client_secret = client_secret,
+        user_agent = user_agent,
     )
     #Raise error if login was unsuccessful
     if login_obj.read_only == False:
@@ -22,13 +22,14 @@ def get_posts(reddit: Login_obj, subreddit:str, limit:int = 1) -> list:
     return list(reddit.subreddit(subreddit).hot(limit=limit))
 
 #get comments from subreddit
-def get_comments(reddit: Login_obj, post_id: str) -> list:
+def get_comments(reddit: Login_obj, post_id: str, limit:int = 10) -> list:
     submission = reddit.submission(id=post_id)
     #get rid of MoreComments class in submission
     submission.comments.replace_more(limit=0)
     submission.comment_sort = "top"
+    #TODO: optimize speed of getting comments
     comments = submission.comments.list()
-    return comments[:10]
+    return comments[:limit]
 
 
 if __name__ == "__main__":
