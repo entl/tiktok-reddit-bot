dddd44ae0537e6fcabc89f177b0d176481c157a5
entl <maxvorobyov01@gmail.com>
2023-01-30 23:52:00 +0000

    get rid of useless imports

--- syntesize.py
+++ syntesize.py
@@ -1,9 +1,8 @@
-from settings import AUDIO_FOLDER
 import os
 from TTS.api import TTS
 
-def syntesize(text:str, filename:str):
+def syntesize(text:str, path: str, filename:str):
     # Init TTS with the target model name
     filename = f'{filename}.mp3'
     tts = TTS(model_name="tts_models/en/ljspeech/tacotron2-DDC", progress_bar=False, gpu=False)
-    tts.tts_to_file(text=text, file_path=os.path.join(AUDIO_FOLDER, filename))
+    tts.tts_to_file(text=text, file_path=os.path.join(path, filename))
