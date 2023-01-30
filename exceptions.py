1c8cc4ab621a4dc8bb53b2cb421506801f1397fe
entl <maxvorobyov01@gmail.com>
2023-01-30 23:48:55 +0000

    bug fix

--- exceptions.py
+++ exceptions.py
@@ -1,28 +1,27 @@
 class CantLoginReddit(Exception):
-    def __init__(self, message):
-        self.message = f'Login is unsuccessful. Error: {message}'
+    def __init__(self):
+        self.message = f'Login is unsuccessful.'
     
     def __str__(self) -> str:
         return self.message
 
 class CantAddRecord(Exception):
     def __init__(self, message):
-        self.message = f'Adding record is unsuccessful. Error: {message}'
+        self.message = f'Adding record is unsuccessful.'
     
     def __str__(self) -> str:
         return self.message
 
 class CantUpdateRecord(Exception):
-    def __init__(self, message):
-        self.message = f'Updating record is unsuccessful. Error: {message}'
+    def __init__(self):
+        self.message = f'Updating record is unsuccessful.'
     
     def __str__(self) -> str:
         return self.message
 
-
 class RecordNotExists(Exception):
-    def __init__(self, message):
-        self.message = f'There is no record with provided submission_id. Error: {message}'
+    def __init__(self):
+        self.message = f'There is no record with provided submission_id.'
     
     def __str__(self) -> str:
         return self.message
